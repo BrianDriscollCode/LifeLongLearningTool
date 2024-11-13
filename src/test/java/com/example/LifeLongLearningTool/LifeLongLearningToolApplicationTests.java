404167ac417c2f6ee5d6e3ed76691c20ebd7e7be
Brian Driscoll <67210898+BrianDriscollCode@users.noreply.github.com>
2024-11-12 17:04:43 -0700

    First comit

---
+++ src/test/java/com/example/LifeLongLearningTool/LifeLongLearningToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LifeLongLearningTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeLongLearningToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
